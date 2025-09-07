ebe252d7b55e76432eac0a5d044361d98ee60f96
SerzhSterl <serzhsterl@gmail.com>
2025-09-08 02:48:42 +0300

    Step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,11 +10,21 @@ import java.util.concurrent.Callable;
 @Command(name = "genDiff", description = "Prints the difference between the data as a string.")
 public class App implements Callable<Integer> {
 
+	@Parameters(index = "0", description = "path to first file")
+	private String firstFilePath;
+
+	@Parameters(index = "1", description = "path to second file")
+	private String secondFilePath;
+
 	@Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
 	private boolean helpRequested;
 
 	@Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
 	private boolean versionRequested;
+
+	@Option(names = {"-f", "--format"}, defaultValue = "stylish",
+			description = "output format [default: stylish]")
+	private String formatFile;
 
 	public static void main(String[] args) {
 		CommandLine commandLine = new CommandLine(new App());
